c38379460eb68222da7907e34568869a2455292f
dilhaqued <dilhaqued@gmail.com>
2022-10-16 16:05:58 +0530

    dynamic difficulty and mine rate

--- block.js
+++ block.js
@@ -1,29 +1,46 @@
-const { GENESIS_DATA } = require("./config");
+const { GENESIS_DATA,MINE_RATE } = require("./config");
 const cryptoHash = require("./crypto-hash");
 
 
 class Block {
-    constructor( {timestamp, lastHash, hash, data} ) {
+    constructor( {timestamp, lastHash, hash, data,nonce,difficulty} ) {
       this.timestamp = timestamp;
       this.lastHash = lastHash;
       this.hash = hash;
       this.data = data;
-      
+      this.nonce = nonce;
+      this.difficulty = difficulty;
     }
     static genesis() {
       return new this(GENESIS_DATA);
     }
 
     static mineBlock({ lastBlock, data}){
-      const timestamp = Date.now();
+      let hash,timestamp;
       const lastHash = lastBlock.hash;
+      const { difficulty } = lastBlock;
+      let nonce = 0;
+
+      do {
+        nonce++;
+        timestamp = Date.now();
+        hash = cryptoHash(timestamp, lastHash,data,nonce,difficulty);
+      } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty));
 
       return new this({
         timestamp,
         lastHash,
         data,
-        hash: cryptoHash(timestamp,lastHash,data)
+        difficulty,
+        nonce,
+        hash
       });
     }
+   static adjustDifficulty({ originalBlock,timestamp}) {
+    const { difficulty } = originalBlock;
+
+    if ((timestamp - originalBlock.timestamp) > MINE_RATE) return difficulty - 1;
+    return difficulty + 1;
+   }
 }
 module.exports = Block;
