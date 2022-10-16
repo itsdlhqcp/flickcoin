7315ca9f85ab36758af867499cc373c8970edb5a
dilhaqued <dilhaqued@gmail.com>
2022-10-16 16:33:50 +0530

    dynamic difficulty and mine rate

--- block.test.js
+++ block.test.js
@@ -72,6 +72,12 @@ describe('mineBlock()', () =>{
        expect(minedBlock.hash.substring(0,minedBlock.difficulty))
        .toEqual('0'.repeat(minedBlock.difficulty));
     });
+
+    it('adjusts the difficulty', () => {
+      const possibleResults = [lastBlock.difficulty+1, lastBlock.difficulty-1];
+      expect(possibleResults.includes(minedBlock.difficulty)).toBe(true);
+
+    })
 });
 
  describe('adjustDifficulty()', () => {
@@ -88,6 +94,12 @@ describe('mineBlock()', () =>{
         timestamp: block.timestamp + MINE_RATE + 100
       })).toEqual(block.difficulty-1);
     });
+
+    it('has a lower limit of 1', () => {
+        block.difficulty = -1;
+
+        expect(Block.adjustDifficulty({ originalBlock: block})).toEqual(1);
+    })
   });
 
 });
