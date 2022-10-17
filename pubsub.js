b2b501bf2d1eb207f22728f737a201a7914530b7
dilhaqued <dilhaqued@gmail.com>
2022-10-17 18:15:44 +0530

    broadcast chain live with pubnub

--- pubsub.js
+++ pubsub.js
@@ -71,6 +71,59 @@
 
 
 // const redis = require('redis');
+// const PubNub = require('pubnub');
+
+// const credentials = {
+//   publishKey: 'pub-c-cecc76a9-a9a8-4448-8c3f-3464c116fc33',
+//   subscribeKey: 'sub-c-c360f9ad-4af5-4ed8-bce0-8a184c13a51f',
+//   secretKey: 'sec-c-MGIxNDk1ZTItMGYxYS00YWRmLWE0N2YtNDA5OGRmMjRkNTZk'
+// };
+
+// const CHANNELS = {
+//   TEST: 'TEST',
+//   BLOCKCHAIN: 'BLOCKCHAIN'
+// };
+
+// class PubSub {
+//   constructor() {
+//     // this.publisher = redis.createClient();
+//     // this.subscriber = redis.createClient();
+//     this.pubnub = new PubNub(credentials);
+
+//     this.pubnub.subscribe({ channels: Object.values(CHANNELS) });
+
+//     this.pubnub.addListener(this.listener());
+//   }
+
+//     // this.subscriber.subscribe(CHANNELS.TEST);
+//   listener() {
+//     return {
+//       message: messageObject => {
+//         const { channel, message } = messageObject;
+
+//     // this.subscriber.on(
+//     //   'message',
+//     //   (channel, message) => this.handleMessage(channel, message)
+//     // );
+//         console.log(`Message received. Channel: ${channel}. Message; ${message}`);
+//       }
+//     };
+//   }
+
+// //   handleMessage(channel, message) {
+// //     console.log(`Message received. Channel: ${channel}. Message: ${message}`);
+//   publish({ channel, message }) {
+//     this.pubnub.publish({ channel, message });
+//   }
+// }
+
+// // const testPubSub = new PubSub();
+// // testPubSub.publish({ channel: CHANNELS.TEST, message: 'hello pubnub' });
+
+// // setTimeout(() => testPubSub.publisher.publish(CHANNELS.TEST, 'foo'), 1000);
+// module.exports = PubSub;
+
+
 const PubNub = require('pubnub');
 
 const credentials = {
@@ -80,44 +133,70 @@ const credentials = {
 };
 
 const CHANNELS = {
-  TEST: 'TEST'
+  TEST: 'TEST',
+  BLOCKCHAIN: 'BLOCKCHAIN',
+  TRANSACTION: 'TRANSACTION'
 };
 
 class PubSub {
-  constructor() {
-    // this.publisher = redis.createClient();
-    // this.subscriber = redis.createClient();
+  constructor({ blockchain, transactionPool }) {
+    this.blockchain = blockchain;
+    this.transactionPool = transactionPool;
+
     this.pubnub = new PubNub(credentials);
-
     this.pubnub.subscribe({ channels: Object.values(CHANNELS) });
-
     this.pubnub.addListener(this.listener());
   }
 
-    // this.subscriber.subscribe(CHANNELS.TEST);
+  handleMessage(channel, message) {
+    console.log(`Message received. Channel: ${channel}. Message: ${message}`);
+
+    const parsedMessage = JSON.parse(message);
+
+    switch(channel) {
+      case CHANNELS.BLOCKCHAIN:
+        this.blockchain.replaceChain(parsedMessage, true, () => {
+          this.transactionPool.clearBlockchainTransactions({
+             chain: parsedMessage
+          });
+        });
+        break;
+      case CHANNELS.TRANSACTION:
+        this.transactionPool.setTransaction(parsedMessage);
+        break;
+      default:
+        return;
+    }
+  }
+
+
   listener() {
     return {
       message: messageObject => {
         const { channel, message } = messageObject;
 
-    // this.subscriber.on(
-    //   'message',
-    //   (channel, message) => this.handleMessage(channel, message)
-    // );
-        console.log(`Message received. Channel: ${channel}. Message; ${message}`);
+        this.handleMessage(channel, message);
       }
     };
   }
 
-//   handleMessage(channel, message) {
-//     console.log(`Message received. Channel: ${channel}. Message: ${message}`);
-  publish({ channel, message }) {
+  publish({ channel, message}) {
     this.pubnub.publish({ channel, message });
+  }
+
+  broadcastChain() {
+    this.publish({
+      channel: CHANNELS.BLOCKCHAIN,
+      message: JSON.stringify(this.blockchain.chain)
+    });
+  }
+
+  broadcastTransaction(transaction) {
+    this.publish({
+      channel: CHANNELS.TRANSACTION,
+      message: JSON.stringify(transaction)
+    })
   }
 }
 
-const testPubSub = new PubSub();
-testPubSub.publish({ channel: CHANNELS.TEST, message: 'hello pubnub' });
-
-// setTimeout(() => testPubSub.publisher.publish(CHANNELS.TEST, 'foo'), 1000);
 module.exports = PubSub;
