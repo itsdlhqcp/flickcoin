92953ae527145af850305ad15b5b634295dde66f
dilhaqued <dilhaqued@gmail.com>
2022-10-17 23:15:21 +0530

    SYN THE BLOCKS OF CHAIN and centrally update over chain peer to peer

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const request = require('request');
 const Blockchain = require('./blockchain');
 const PubSub = require('./pubsub');
 
 const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
+
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
 setTimeout(() => pubsub.broadcastChain(), 1000);
 
@@ -25,8 +29,28 @@ app.post('/api/mine', (req, res) => {
     res.redirect('/api/blocks');
   });
 
-const PORT = 3000;
+  const syncChains = () => {
+    request({ url: `${ROOT_NODE_ADDRESS}/api/blocks` }, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const rootChain = JSON.parse(body);
+  
+        console.log('replace chain on a sync with', rootChain);
+        blockchain.replaceChain(rootChain);
+      }
+    });
+  };
+
+let PEER_PORT;
+
+if (process.env.GENERATE_PEER_PORT === 'true') {
+  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT
 app.listen(PORT, () => {
   console.log(`listening at localhost:${PORT}`);
+
+  syncChains();
 });
 
